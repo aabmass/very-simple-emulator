c03625767e916ac7053557381176b4a14008ab88
Aaron Abbott <aabmass@gmail.com>
2016-11-25 13:46:50 -0500

    CMake build a library

--- util.hpp
+++ util.hpp
@@ -4,6 +4,7 @@
 #include "types.hpp"
 #include <vector>
 
+std::string load_text_file(std::string const& fname);
 std::vector<Byte> load_binary_file(std::string const& fname);
 Address create_addr_from_bytes(Byte addr_low, Byte addr_high);
 
